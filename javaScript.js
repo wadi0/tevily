ea34114c2edb0060dd09bc2f02f123829ac76997
Your User Name <zabir4897@gmail.com>
2024-05-17 23:57:40 +0600

    first slider done

--- javaScript.js
+++ javaScript.js
@@ -76,3 +76,33 @@ window.addEventListener('scroll', function () {
 //         headerMenu.classList.add('navbar-fixed');
 //     }
 // });
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+      autoplay: {
+    delay: 5000, // milliseconds
+    disableOnInteraction: false, // prevent auto-play from stopping on user interaction
+  },
+
+    slidesPerView: 5,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
